0da35fbe10953982df511f3572c8b75aec643c2b
José Pedro <pedroosd28@gmail.com>
2026-01-29 23:28:10 -0300

    feat(transactions): update actor and transaction repositories

--- backend/modules/transactions/repositories/actor.py
+++ backend/modules/transactions/repositories/actor.py
@@ -7,13 +7,14 @@ class ActorRepository:
     def __init__(self, model: Actor, actor_factory: ActorFactory):
         self.model = model
         self.actor_factory = actor_factory
+        self.queryset = self.model.objects.order_by("name")
 
     def get(self, actor_id: str, user_id: int) -> ActorDomain:
-        actor_instance = self.model.objects.get(id=actor_id, user_id=user_id)
+        actor_instance = self.queryset.get(id=actor_id, user_id=user_id)
         return self.actor_factory.build_from_model(actor_instance)
 
     def get_all(self, user_id: int) -> list[ActorDomain]:
-        actor_instances = self.model.objects.filter(user_id=user_id)
+        actor_instances = self.queryset.filter(user_id=user_id)
         return [self.actor_factory.build_from_model(actor) for actor in actor_instances]
 
     def create(self, actor: ActorDomain) -> ActorDomain:
@@ -24,10 +25,10 @@ class ActorRepository:
         return self.actor_factory.build_from_model(actor_instance)
 
     def update(self, actor: ActorDomain) -> ActorDomain:
-        actor_instance = self.model.objects.get(id=actor.id, user_id=actor.user_id)
+        actor_instance = self.queryset.get(id=actor.id, user_id=actor.user_id)
         actor_instance.name = actor.name
         actor_instance.save()
         return self.actor_factory.build_from_model(actor_instance)
 
     def delete(self, actor_id: str, user_id: int):
-        self.model.objects.get(id=actor_id, user_id=user_id).delete()
+        self.queryset.get(id=actor_id, user_id=user_id).delete()
--- backend/modules/transactions/repositories/transaction.py
+++ backend/modules/transactions/repositories/transaction.py
@@ -7,17 +7,18 @@ class TransactionRepository:
     def __init__(self, model: Transaction, transaction_factory: TransactionFactory):
         self.model = model
         self.transaction_factory = transaction_factory
+        self.queryset = self.model.objects.order_by("id")
 
     def filter(self, filters: dict) -> list["TransactionDomain"]:
-        queryset = self.model.objects.filter(**filters)
+        queryset = self.queryset.filter(**filters)
         return [self.transaction_factory.build_from_model(transaction) for transaction in queryset]
 
     def get(self, transaction_id: str, user_id: int) -> "TransactionDomain":
-        transaction_instance = self.model.objects.get(id=transaction_id, user_id=user_id)
+        transaction_instance = self.queryset.get(id=transaction_id, user_id=user_id)
         return self.transaction_factory.build_from_model(transaction_instance)
     
     def get_all(self, user_id: int) -> list["TransactionDomain"]:
-        transaction_instances = self.model.objects.filter(user_id=user_id)
+        transaction_instances = self.queryset.filter(user_id=user_id)
         return [self.transaction_factory.build_from_model(transaction) for transaction in transaction_instances]
     
     def create(self, transaction: "TransactionDomain") -> "TransactionDomain":
@@ -36,7 +37,7 @@ class TransactionRepository:
         return self.transaction_factory.build_from_model(transaction_instance)
     
     def update(self, transaction: "TransactionDomain") -> "TransactionDomain":
-        transaction_instance = self.model.objects.get(id=transaction.id, user_id=transaction.user_id)
+        transaction_instance = self.queryset.get(id=transaction.id, user_id=transaction.user_id)
         transaction_instance.due_date = transaction.due_date
         transaction_instance.total_amount = transaction.total_amount
         transaction_instance.transaction_identifier = transaction.transaction_identifier
@@ -47,5 +48,5 @@ class TransactionRepository:
         return self.transaction_factory.build_from_model(transaction_instance)
     
     def delete(self, transaction_id: str, user_id: int):
-        self.model.objects.get(id=transaction_id, user_id=user_id).delete()
+        self.queryset.get(id=transaction_id, user_id=user_id).delete()
 
