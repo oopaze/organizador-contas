3cef55520fc494491192700dc7aa7cdca7e282aa
José Pedro <pedroosd28@gmail.com>
2026-02-01 01:23:39 -0300

    fix: Add STATIC_ROOT for collectstatic in production

--- backend/infra/settings.py
+++ backend/infra/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Custom User Model
 AUTH_USER_MODEL = "userdata.User"
