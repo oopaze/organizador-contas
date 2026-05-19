8fd5d765e42f2db7e9cb8d97db4a7bbf8ec35f34
José Pedro <pedroosd28@gmail.com>
2026-05-19 14:04:03 -0300

    feat(loans): loan stats use case

--- backend/modules/loans/use_cases/loan/__init__.py
+++ backend/modules/loans/use_cases/loan/__init__.py
@@ -3,6 +3,7 @@ from modules.loans.use_cases.loan.get import GetLoanUseCase
 from modules.loans.use_cases.loan.list import ListLoansUseCase
 from modules.loans.use_cases.loan.update import UpdateLoanUseCase
 from modules.loans.use_cases.loan.delete import DeleteLoanUseCase
+from modules.loans.use_cases.loan.stats import LoanStatsUseCase
 
 __all__ = [
     "CreateLoanUseCase",
@@ -10,4 +11,5 @@ __all__ = [
     "ListLoansUseCase",
     "UpdateLoanUseCase",
     "DeleteLoanUseCase",
+    "LoanStatsUseCase",
 ]
